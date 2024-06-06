1231b6d770d9203acdd5b8b1eacd21369fefc821
rhrynko-intellias <roman.hrynko@intellias.com>
2024-06-06 18:47:57 +0300

    Added "All photos" btn

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -1,23 +1,44 @@
-import { ImageList, ImageListItem } from "@mui/material";
+import { Box, Button, ImageList, ImageListItem, Typography } from "@mui/material";
+import AppsIcon from '@mui/icons-material/Apps';
 
 export const ImageGallery = () => {
   return (
-    <ImageList
-      sx={{ width: "100%", height: 400, borderRadius: 5, overflow: "hidden" }}
-      variant="quilted"
-      cols={4}
-      rowHeight={200}
-    >
-      {itemData.map((item) => (
-        <ImageListItem key={item.img} cols={item.cols || 1} rows={item.rows || 1}>
-          <img
-            {...srcset(item.img, 121, item.rows, item.cols)}
-            alt={item.title}
-            loading="lazy"
-          />
-        </ImageListItem>
-      ))}
-    </ImageList>
+    <Box position="relative">
+      <ImageList
+        sx={{ width: "100%", height: 400, borderRadius: 5, overflow: "hidden" }}
+        variant="quilted"
+        cols={4}
+        rowHeight={200}
+      >
+        {itemData.map((item) => (
+          <ImageListItem key={item.img} cols={item.cols || 1} rows={item.rows || 1}>
+            <img
+              {...srcset(item.img, 121, item.rows, item.cols)}
+              alt={item.title}
+              loading="lazy"
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>
+      <Button
+        sx={{
+          position: "absolute",
+          bottom: 10,
+          right: 10,
+          width: "120px",
+          height: "30px",
+          color: "white",
+          backgroundColor: "rgba(136, 136, 136, 0.3)",
+          borderRadius: 5,
+          textTransform: "none",
+          "&:hover": {
+            backgroundColor: "rgba(136, 136, 136, 0.8)",
+          }
+        }}
+        startIcon={<AppsIcon />} >
+        <Typography variant="body2">All photos</Typography>
+      </Button>
+    </Box>
   );
 };
 
