567b3113ee1e694588cf0bd9a21be31f7da40151
rhrynko-intellias <roman.hrynko@intellias.com>
2024-06-10 19:35:16 +0300

    Add GoldenRegion component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Box } from '@mui/material';
 import { Header } from './shell/Header';
 import { TripOverview } from './trip-overview/TripOverview';
 import { IcelandHighLights } from './iceland-highlight/IcelandHighLights';
+import { GoldenRegion } from './regions/GoldenRegion';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Box p={{ xs: 2, lg: 4 }}>
         <TripOverview />
         <IcelandHighLights />
+        <GoldenRegion />
       </Box>
     </Box>
   );
